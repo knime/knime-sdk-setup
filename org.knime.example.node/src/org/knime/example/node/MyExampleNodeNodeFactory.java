141056a0f64a09704e09a6680508b09cdbef11f4
dietzc <christian.dietz@knime.com>
2017-02-27 16:25:36 -0600

    add example node project

---
+++ org.knime.example.node/src/org/knime/example/node/MyExampleNodeNodeFactory.java
@@ -0,0 +1,58 @@
+package org.knime.example.node;
+
+import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeFactory;
+import org.knime.core.node.NodeView;
+
+/**
+ * <code>NodeFactory</code> for the "MyExampleNode" Node.
+ * This is an example node provided by KNIME.com.
+ *
+ * @author KNIME.com
+ */
+public class MyExampleNodeNodeFactory 
+        extends NodeFactory<MyExampleNodeNodeModel> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public MyExampleNodeNodeModel createNodeModel() {
+        return new MyExampleNodeNodeModel();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getNrNodeViews() {
+        return 1;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeView<MyExampleNodeNodeModel> createNodeView(final int viewIndex,
+            final MyExampleNodeNodeModel nodeModel) {
+        return new MyExampleNodeNodeView(nodeModel);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean hasDialog() {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeDialogPane createNodeDialogPane() {
+        return new MyExampleNodeNodeDialog();
+    }
+
+}
+
